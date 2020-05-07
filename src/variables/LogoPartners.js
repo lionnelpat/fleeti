0a637176a0ce954b8f5b8e96bc4894f5ab2042d0
kheuch2020 <cheikh@mobility-sn.com>
2020-05-07 10:37:53 +0000

    not-responsive

--- src/variables/LogoPartners.js
+++ src/variables/LogoPartners.js
@@ -1,11 +1,11 @@
 import logo1_couleur from "../img/logo1_couleur.jpg"
 import logo2_couleur from "../img/logo2_couleur.png"
 import logo3_couleur from "../img/logo3_couleur.png"
-import logo4_couleur from "../img/logo4_couleur.jpg"
+//import logo4_couleur from "../img/logo4_couleur.jpg"
 import logo5_couleur from "../img/logo5_couleur.png"
 import logo6_couleur from "../img/logo6_couleur.jpg"
 import logo7_couleur from "../img/logo7_couleur.png"
 
 
 
-export const logos = [logo1_couleur,logo2_couleur,logo3_couleur,logo4_couleur,logo5_couleur,logo6_couleur,logo7_couleur]
+export const logos = [logo1_couleur,logo2_couleur,logo3_couleur,logo5_couleur,logo6_couleur,logo7_couleur]
