ff043c394a01d1bb0c312dfffd9664b203204373
kheuch2020 <cheikh@mobility-sn.com>
2020-05-08 11:36:40 +0000

    espace bande outils

--- src/components/Bande2.js
+++ src/components/Bande2.js
@@ -12,7 +12,7 @@ const Bande2 = () => {
                     <h1 style={{ fontSize: 50, color: "#0BA1C1" }}>Fleeti s'intègre à vos outils</h1>
                     <p style={{ fontSize: 20, color: "#757575" }}>Nous développons des passerelles entre vos logiciels et notre outils, pas besoin de changer vos habitudes !</p>
 
-                    <div style={{ marginTop: 80 }}>
+                    <div style={{ marginTop: 10 }}>
                         <Link to="/demo-gratuite">
                         <Button style={{ backgroundColor: "#0BA1C1", color: "white", fontWeight: "bold" }} size="big" >Démo gratuite</Button>
                         </Link>
