2c9415607bcf8f8eb7180644187db56fa1ed1cb8
nina-tchalla <nina@mobility-sn.com>
2020-05-13 21:19:14 +0000

    Fixed some issues

--- src/components/Grille.js
+++ src/components/Grille.js
@@ -34,10 +34,10 @@ const Grille = () => {
                     <Grid.Row style={{ display: "flex", justifyContent: "center" }}>
                         <div style={{ width: "50%", display: "flex", position: "relative", marginTop: 50 }} >
                             <Image src={quote} style={{ position: "absolute", top: -35, left: -70 }} />
-                            <p id="grText2" >
+                            <div id="grText2" >
                                 “Bien que nous soyons une PME avec un nombre limité de véhicules, Fleeti a pris le temps de comprendre notre fonctionnement et nous a permis d’économiser plus de 20% sur nos dépenses d’entretien par rapport à l’année dernière” <br />
                                 <p style={{ textAlign: "center", fontWeight: "bold", color: "#0BA1C1", marginTop: 20 }}>F.Baudoin, Soram Ouest Africa</p>
-                            </p>
+                            </div>
                         </div>
 
                     </Grid.Row>
--- src/components/SecondNav.js
+++ src/components/SecondNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Button, Dropdown } from 'semantic-ui-react';
+import { Menu, Button } from 'semantic-ui-react';
 import CustomMenuItem from './CustomMenuItem';
 import { items } from '../variables/SecondNav';
 
@@ -11,13 +11,13 @@ const SecondNav = () => {
     return (
         <Menu.Menu position="right" style={{marginTop: -30}}>
 
-            
+
             {items.map((item,i) => <CustomMenuItem
                 key={i}
                 title={item.title}
                 categories={item.categories}
             />)}
-    
+
             <Menu.Item>
                 <Button style={{backgroundColor: "#FF2407", color:"white",fontWeight:"bold"}} >TARIFS</Button>
             </Menu.Item>
