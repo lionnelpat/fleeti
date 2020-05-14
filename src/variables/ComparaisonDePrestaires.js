8c63ff193a60a5371de425200ecbe9a160baf4ce
CheikhDiengGuye <cheikhd@mobility-sn.com>
2020-05-14 12:52:47 +0000

    images et fonts and global style reviewe

--- src/variables/ComparaisonDePrestaires.js
+++ src/variables/ComparaisonDePrestaires.js
@@ -3,18 +3,18 @@ export const comparaisonDePrestaires =
     data: [
         {
             img: "cdp1",
-            title: "Mettez en concurrence vos fournisseurs ",
-            text: "Notre module permet de comprendre et comparer facilement les offres des prestataires automobiles afin de choisir le meilleur devis en fonction de vos critères (délais, tarifs, avis...). "
+            title: "Mettez en concurrence vos<br/> fournisseurs ",
+            text: "Notre module permet de comprendre et<br/> comparer facilement les offres des<br/> prestataires automobiles afin de choisir le<br/> meilleur devis en fonction de vos critères<br/> (délais, tarifs, avis...). "
         },
         {
             img: "cdp2",
             title: "Gérez vos stocks",
-            text: "Que vous disposiez d’ateliers en propre ou d’un simple stock tampon de pièces détachées, notre outil de vous permet d’en optimiser la gestion via le module de gestion de stocks."
+            text: "Que vous disposiez d’ateliers en propre<br/> ou d’un simple stock tampon de pièces<br/> détachées, notre outil de vous permet d’en<br/> optimiser la gestion via le module de gestion<br/> de stocks."
         },
         {
             img: "cdp3",
-            title: "Profitez de tarifs négociés",
-            text: "S’abonner à Fleeti c’est rejoindre une communauté d’entreprises qui se fournissent via notre outil. Ainsi profitez de tarifs préférentiels sur de nombreuses prestations (entretien, réparation, achat de pièces détachées)"
+            title: "Profitez de tarifs<br/> négociés",
+            text: "S’abonner à Fleeti c’est rejoindre une<br/> communauté d’entreprises qui se fournissent<br/> via notre outil. Ainsi profitez de tarifs<br/> préférentiels sur de nombreuses prestations<br/> (entretien, réparation, achat de pièces<br/> détachées)"
         },
     ],
     title3: "Comparez les services et profitez des meilleurs tarifs de la part <br/> professionels de l’automobile",
