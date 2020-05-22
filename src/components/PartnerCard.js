bc30cd2e03523ddf36ee14cdec2c7b0e9bde3c81
CheikhDiengGuye <cheikhd@mobility-sn.com>
2020-05-22 11:57:17 +0000

    Devenir partenaire page reviewed

--- src/components/PartnerCard.js
+++ src/components/PartnerCard.js
@@ -10,11 +10,10 @@ function createMarkup(title) {
     return { __html: title };
 }
 
-const PartnerCard = ({ title, text, avantages, img, mt, height = 175, link = "/", mt2 }) => {
+const PartnerCard = ({ title, text, avantages, img, mt, height = 175, link = "/", mt2, deviceType }) => {
     return (
-        
-        <>
-        <Responsive maxWidth={1024}>
+
+
         <Grid.Column style={{ textAlign: "center", justifyContent: "center", marginTop: mt2 || mt, height: 450 }}>
             <Grid columns={1}>
                 <Grid.Row>
@@ -33,8 +32,8 @@ const PartnerCard = ({ title, text, avantages, img, mt, height = 175, link = "/"
 
                 <Grid.Row style={{ paddingTop: 0 }}>
                     <Grid.Column>
-                        <p style={{ color: '#000000', fontSize: 14, fontFamily: 'Muli', marginTop: 5, fontWeight: 'bold', textAlign: 'center' }} dangerouslySetInnerHTML={createMarkup(text)}></p>
-                        <p>
+                        <p style={deviceType === 'phone' ? { color: '#000000', fontSize: 14, fontFamily: 'Muli', marginTop: 5, fontWeight: 'bold', textAlign: 'center' } : { color: '#000000', fontSize: 14, fontFamily: 'Muli', marginTop: 5, fontWeight: 'bold', textAlign: 'left', marginLeft: '10%' }} dangerouslySetInnerHTML={createMarkup(text)}></p>
+                        <p style={{ display: 'flex', justifyContent: 'center' }}>
                             <ul style={{ marginLeft: 50, lineHeight: 1.7 }}>
                                 {avantages.map((item, index) => <li style={{ textAlign: 'left', fontSize: 14, fontWeight: '500', fontFamily: 'Muli', color: '#000000' }}>{item}</li>)}
                             </ul>
@@ -44,39 +43,7 @@ const PartnerCard = ({ title, text, avantages, img, mt, height = 175, link = "/"
 
             </Grid>
         </Grid.Column>
-        </Responsive>
-        <Responsive minWidth={1024.999}>
-        <Grid.Column style={{ textAlign: "center", justifyContent: "center", marginTop: mt2 || mt, height: 380 }}>
-            <Grid columns={1}>
-                <Grid.Row>
-                    <Grid.Column style={{ display: "flex", justifyContent: "center" }}>
-                        <div style={{ height }}>
-                            {img === "p1" && <Image src={p1} style={{ width: 160, height: 162.13 }} />}
-                            {img === "p2" && <Image src={p2} style={{ width: 250, height: 166.69 }} />}
-                            {img === "p3" && <Image src={p3} style={{ width: 250, height: 166.88 }} />}
-                        </div>
 
-                    </Grid.Column>
-                    <Grid.Column style={{ bottom: 0 }} >
-                        <p style={{ color: 'rgba(220, 165, 20, 0.96)', fontWeight: 'bold', fontSize: 24, fontFamily: 'Muli', marginTop: 15 }} dangerouslySetInnerHTML={createMarkup(title)}></p>
-                    </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row style={{ paddingTop: 0 }}>
-                    <Grid.Column>
-                        <p style={{ color: '#000000', fontSize: 14, fontFamily: 'Muli', marginTop: 5, fontWeight: 'bold', textAlign: 'left', marginLeft: 55 }} dangerouslySetInnerHTML={createMarkup(text)}></p>
-                        <p>
-                            <ul style={{ marginLeft: 74, lineHeight: 1.7 }}>
-                                {avantages.map((item, index) => <li style={{ textAlign: 'left', fontSize: 14, fontWeight: '500', fontFamily: 'Muli', color: '#000000' }}>{item}</li>)}
-                            </ul>
-                        </p>
-                    </Grid.Column>
-                </Grid.Row>
-
-            </Grid>
-        </Grid.Column>
-        </Responsive>
-        </>
     );
 };
 
--- src/components/PartnerCardGroup.js
+++ src/components/PartnerCardGroup.js
@@ -7,36 +7,38 @@ const PartnerCardGroup = ({ data = [] }) => {
     return (
         <>
             <Responsive maxWidth={1024}>
-            <Card style={{ width: "100%", paddingLeft: 50, paddingRight: 50, paddingTop: 10, boxShadow: "none" }} centered>
-                <Grid columns={1} >
-                    <Grid.Row>
-                        {data.map((item, i) => <PartnerCard
-                            key={i}
-                            img={item.img}
-                            title={item.title}
-                            text={item.text}
-                            avantages={item.avantages}
-                        />)}
+                <Card style={{ width: "100%", paddingLeft: 50, paddingRight: 50, paddingTop: 10, boxShadow: "none" }} centered>
+                    <Grid columns={1} >
+                        <Grid.Row>
+                            {data.map((item, i) => <PartnerCard
+                                key={i}
+                                img={item.img}
+                                title={item.title}
+                                text={item.text}
+                                avantages={item.avantages}
+                                deviceType={'phone'}
+                            />)}
 
-                    </Grid.Row>
-                </Grid>
-            </Card>
+                        </Grid.Row>
+                    </Grid>
+                </Card>
             </Responsive>
             <Responsive minWidth={1024.999}>
-            <Card style={{ width: "100%", paddingLeft: 50, paddingRight: 50, paddingTop: 10, marginBottom: 100, boxShadow: "none" }} centered>
-                <Grid columns={3} >
-                    <Grid.Row>
-                        {data.map((item, i) => <PartnerCard
-                            key={i}
-                            img={item.img}
-                            title={item.title}
-                            text={item.text}
-                            avantages={item.avantages}
-                        />)}
+                <Card style={{ width: "100%", paddingLeft: 50, paddingRight: 50, paddingTop: 10, marginBottom: 100, boxShadow: "none" }} centered>
+                    <Grid columns={3} >
+                        <Grid.Row>
+                            {data.map((item, i) => <PartnerCard
+                                key={i}
+                                img={item.img}
+                                title={item.title}
+                                text={item.text}
+                                avantages={item.avantages}
+                                deviceType={'desktop'}
+                            />)}
 
-                    </Grid.Row>
-                </Grid>
-            </Card>
+                        </Grid.Row>
+                    </Grid>
+                </Card>
             </Responsive>
 
         </>
